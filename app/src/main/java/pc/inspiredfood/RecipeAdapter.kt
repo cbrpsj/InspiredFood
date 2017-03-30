560276c3db891fc23309c4ed7a14976824d741d6
psj1980 <vi.har.det.godt@gmail.com>
2017-03-30 21:20:11 +0200

    Cursor adapter replaced by Array adapter

--- app/src/main/java/pc/inspiredfood/RecipeAdapter.kt
+++ app/src/main/java/pc/inspiredfood/RecipeAdapter.kt
@@ -5,11 +5,37 @@ import android.database.Cursor
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.CursorAdapter
 import kotlinx.android.synthetic.main.item_recipe.view.*
 
 
-class RecipeAdapter(context: Context, cursor: Cursor, flags: Int) : CursorAdapter(context, cursor, flags) {
+// Map data from a collection to the elements in the listView
+class RecipeAdapter(context: Context, val recipes:List<Pair<Int, String>>):
+        ArrayAdapter<Pair<Int, String>>(context, 0, recipes) {
+
+    // For each element getView is called
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        // Find the next recipe based on the position
+        val recipe = recipes[position]
+
+        // The first time, convertView is null and is created
+        val view = convertView ?: LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_recipe, parent, false)
+
+        // Set tag to id (first var in Pair), and display the recipeName in view (second var in Pair)
+        view.tag = recipe.first
+        view.textRecipeMain.setText(recipe.second)
+
+        // return the view
+        return view
+    }
+}
+
+// Map data from a cursor to the elements in the listView
+class RecipeCursorAdapter(context: Context, cursor: Cursor, flags: Int) : CursorAdapter(context, cursor, flags) {
 
     override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
 
@@ -20,8 +46,7 @@ class RecipeAdapter(context: Context, cursor: Cursor, flags: Int) : CursorAdapte
 
     override fun bindView(view: View, context: Context, cursor: Cursor) {
 
-        view.tag = cursor.getColumnIndex(C.RecipesTable.id)
-
+        view.tag = cursor.getInt(cursor.getColumnIndex(C.RecipesTable.id))
         view.textRecipeMain.setText(cursor.getString(cursor.getColumnIndex(C.RecipesTable.recipeName)))
     }
 }
--- app/src/main/java/pc/inspiredfood/RecipeListActivity.kt
+++ app/src/main/java/pc/inspiredfood/RecipeListActivity.kt
@@ -3,9 +3,15 @@ package pc.inspiredfood
 import android.app.Activity
 import android.app.ListActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_recipe_list.*
+import org.jetbrains.anko.db.*
+import org.jetbrains.anko.toast
+
 
 class RecipeListActivity : ListActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -15,15 +21,56 @@ class RecipeListActivity : ListActivity() {
         mainButtons.check(all.id)
     }
 
+
     override fun onResume() {
 
         super.onResume()
 
         RecipeDBHelper.instance.use {
 
-            val cursor = rawQuery("select * from ${C.RecipesTable.tableName}", null)
-            listAdapter = RecipeAdapter(this@RecipeListActivity, cursor, 0)
+            //val cursor = rawQuery("select * from ${C.RecipesTable.tableName}", null)
+            //listAdapter = RecipeCursorAdapter(this@RecipeListActivity, cursor, 0)
 
+            // Create a row parser (parse all fields, and return two of them as a Pair)
+            val parser = rowParser {
+
+                id: Int,
+                recipeName: String,
+                category: Int,
+                instr: String,
+                popul: Int,
+                noOfPeople: Int -> Pair(id, recipeName)
+            }
+
+            // Query db for all recipes, orderBy recipeName and parse the result to a list
+            val recipes = select(C.RecipesTable.tableName)
+                    .orderBy(C.RecipesTable.recipeName)
+                    .parseList(parser)
+
+            listAdapter = RecipeAdapter(this@RecipeListActivity, recipes)
         }
+    }
+
+    override fun onListItemClick(listView: ListView?, view: View, position: Int, id: Long) {
+
+        val recipeId = view.tag
+
+        if (recipeId is Int)
+            toast("RecipeID: $recipeId")
+    }
+
+    fun mainButtonClicked(view: View) {
+
+        var str = ""
+
+        when(view.id) {
+            starters.id -> str = "Starters"
+            mains.id -> str = "Mains"
+            desserts.id -> str = "Desserts"
+            favourites.id -> str = "Favourites"
+            all.id -> str = "All"
+        }
+
+        toast(str)
     }
 }
