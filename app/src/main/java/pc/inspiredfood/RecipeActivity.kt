a669636914624b5398fffde20afc2350e3c4487a
Pia Jensen <vi.har.det.godt@gmail.com>
2017-04-08 19:12:19 +0200

    table layout in RecipeActivity updated

--- app/src/main/java/pc/inspiredfood/RecipeActivity.kt
+++ app/src/main/java/pc/inspiredfood/RecipeActivity.kt
@@ -2,6 +2,7 @@ package pc.inspiredfood
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TableRow
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_recipe.*
@@ -18,43 +19,48 @@ class RecipeActivity : Activity() {
         setContentView(R.layout.activity_recipe)
     }
 
+
     override fun onResume() {
 
         super.onResume()
         val id = intent.getIntExtra(C.recipeId, -1)
-        fetchRecipe(id)
-        createTableRows(id)
+        getRecipeDetails(id)
+        getIngredientsForRecipe(id)
     }
 
-    fun fetchRecipe(id: Int) {
+
+    fun getRecipeDetails(id: Int) {
 
         var recipeName: String
         var category: String
-        var instructions: String
+        var preparation: String
         var popularity: Int
         var numberOfPeople: Int
 
         RecipeDBHelper.instance.use {
 
-            // Query db for all recipes, orderBy recipeName and parse the result to a list
-            val recipe = select(C.RecipesTable.tableName+","+C.CategoriesTable.tableName,
+            // Query db for all recipes, orderBy recipeName and parse result to a list
+            val recipe = select(
+                    C.RecipesTable.tableName+","+C.CategoriesTable.tableName,
                     C.RecipesTable.recipeName, C.CategoriesTable.categoryName, C.RecipesTable.preparation, C.RecipesTable.numberOfPeople)
-                    .where("$id = ${C.RecipesTable.tableName}.${C.RecipesTable.id} and " +
-                            "${C.RecipesTable.tableName}.${C.RecipesTable.category} = ${C.CategoriesTable.tableName}.${C.CategoriesTable.id}")
-                    .parseSingle(rowParser { recipeName: String, categoryName: String, instructions: String, numberOfPeople: Int ->
-                        recipe_name.setText(recipeName)
+                    .where("$id = " +
+                            "${C.RecipesTable.tableName}.${C.RecipesTable.id} and " +
+                            "${C.RecipesTable.tableName}.${C.RecipesTable.category} = " +
+                            "${C.CategoriesTable.tableName}.${C.CategoriesTable.id}")
+                    .parseSingle(rowParser {
+                        recipeName: String, categoryName: String, preparation: String, numberOfPeople: Int ->
 
                         var recipeInfo = "${translateCategory(categoryName)} ${getString(R.string.recipe_info_for)} $numberOfPeople"
+                        if (numberOfPeople > 1) recipeInfo += " ${getString(R.string.recipe_info_persons)}"
+                        else recipeInfo += " ${getString(R.string.recipe_info_person)}"
 
-                        if (numberOfPeople > 1)
-                            recipe_info.setText("$recipeInfo ${getString(R.string.recipe_info_persons)}")
-                        else
-                            recipe_info.setText("$recipeInfo ${getString(R.string.recipe_info_person)}")
-
-                        recipe_instructions.setText(instructions)
+                        recipe_name.text = recipeName
+                        recipe_info.text = recipeInfo
+                        recipe_preparation.text = preparation
                     })
         }
     }
+
 
     fun translateCategory(categoryName: String): String =
             when(categoryName){
@@ -63,12 +69,18 @@ class RecipeActivity : Activity() {
                 else      -> getString(R.string.dessert)
             }
 
-    fun createTableRows(id: Int) {
+
+    // Get ingredients for a specific recipe
+    fun getIngredientsForRecipe(id: Int) {
+
+        var ingredientsInRecipe = listOf<Triple<String, Double, String>>()
 
         RecipeDBHelper.instance.use {
 
-            val ingredientsInRecipe = select(C.IngredientsInRecipesTable.tableName+","+C.IngredientsTable.tableName+","+
-                C.UnitsTable.tableName, C.IngredientsTable.ingredientName, C.IngredientsInRecipesTable.amount, C.UnitsTable.unitName)
+            // Query db for all ingredients in a recipe and parse result to list of Triples
+            ingredientsInRecipe = select(
+                    C.IngredientsInRecipesTable.tableName+","+C.IngredientsTable.tableName+","+
+                            C.UnitsTable.tableName, C.IngredientsTable.ingredientName, C.IngredientsInRecipesTable.amount, C.UnitsTable.unitName)
                     .where("$id = " +
                             "${C.IngredientsInRecipesTable.tableName}.${C.IngredientsInRecipesTable.recipeId} and " +
                             "${C.IngredientsInRecipesTable.tableName}.${C.IngredientsInRecipesTable.ingredientId} =" +
@@ -79,31 +91,44 @@ class RecipeActivity : Activity() {
                         ingredientName: String, amount: Double, unit: String ->
                         Triple(ingredientName, amount, unit)
                     })
-
-            for(ingredientLine in ingredientsInRecipe) {
-                val tableRow = TableRow(this@RecipeActivity)
-                val textViewIngredient = TextView(this@RecipeActivity)
-                val textViewAmount = TextView(this@RecipeActivity)
-                val textViewUnit = TextView(this@RecipeActivity)
-
-                textViewIngredient.setText(ingredientLine.first)
-                textViewAmount.setText(ingredientLine.second.toString())
-                textViewUnit.setText(ingredientLine.third)
-
-                tableRow.addView(textViewIngredient)
-                tableRow.addView(textViewAmount)
-                tableRow.addView(textViewUnit)
-                ingredients_table.addView(tableRow)
-            }
         }
 
+        createTableRows(ingredientsInRecipe)
+    }
 
 
+    // Create table rows and insert data in rows
+    fun createTableRows(ingredientsInRecipe: List<Triple<String, Double, String>>) {
 
-//        textViewIngredient.setPadding(20, 10, 20, 10)
+        // Create a table row foreach element in the list and display ingredient info
+        for(ingredientLine in ingredientsInRecipe) {
 
+            // Create table row
+            val tableRow = TableRow(this)
+            val lp = TableRow.LayoutParams()
+            lp.weight = 8f
 
+            // Create text views for table row
+            val textViewIngredient = TextView(this)
+            val textViewAmount = TextView(this)
+            val textViewUnit = TextView(this)
 
+            // Insert text in text views
+            textViewIngredient.text = ingredientLine.first
+            textViewAmount.text = ingredientLine.second.toString()
+            textViewUnit.text =ingredientLine.third
 
+            // Set padding, text alignment and weight for text views
+            textViewIngredient.layoutParams = lp
+            textViewIngredient.setPadding(0, 0, 40, 0)
+            textViewAmount.gravity = Gravity.END
+            textViewUnit.setPadding(50, 0, 10, 0)
+
+            // Add text views to table row, and add table row to table layout
+            tableRow.addView(textViewIngredient)
+            tableRow.addView(textViewAmount)
+            tableRow.addView(textViewUnit)
+            ingredients_table.addView(tableRow)
+        }
     }
 }
